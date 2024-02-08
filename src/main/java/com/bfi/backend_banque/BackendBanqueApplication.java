4c23c740705939d95bd3fc75478531b3620334b7
cheybah <cheymabahroun@gmail.com>
2024-02-08 10:51:57 +0100

    first commit

---
+++ src/main/java/com/bfi/backend_banque/BackendBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.bfi.backend_banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBanqueApplication.class, args);
+	}
+
+}
