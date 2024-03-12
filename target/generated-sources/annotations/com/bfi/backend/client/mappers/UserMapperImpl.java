d5d7498e19d3c9ec868cd8d05a0398eb062af4a9
cheybah <cheymabahroun@gmail.com>
2024-03-12 13:36:02 +0100

    rbac

--- target/generated-sources/annotations/com/bfi/backend/client/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/bfi/backend/client/mappers/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-03-07T08:46:34+0100",
+    date = "2024-03-08T11:22:53+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
